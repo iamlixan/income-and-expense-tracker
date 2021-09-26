3832cbb36c6cfcdbcf07e7ec12b5137affc3cd66
iamlixan <myselflixan@gmail.com>
2021-09-26 20:25:08 +0800

    added add button UI behaviors

--- app/src/main/java/com/lixan/fajardo/incomeandexpensetracker/main/home/HomeActivity.kt
+++ app/src/main/java/com/lixan/fajardo/incomeandexpensetracker/main/home/HomeActivity.kt
@@ -4,18 +4,30 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.lixan.fajardo.incomeandexpensetracker.R
 import com.lixan.fajardo.incomeandexpensetracker.databinding.ActivityHomeBinding
 import com.lixan.fajardo.incomeandexpensetracker.di.base.BaseViewModelActivity
+import com.lixan.fajardo.incomeandexpensetracker.ext.ninjaTap
 
 class HomeActivity : BaseViewModelActivity<ActivityHomeBinding, HomeViewModel>() {
 
     override fun getLayoutId(): Int = R.layout.activity_home
 
+    private val fromBottomAnim : Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.anim_from_bottom) }
+    private val toBottomAnim : Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.anim_to_bottom) }
+    private val openAnim : Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.anim_rotate_open) }
+    private val closeAnim : Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.anim_rotate_close) }
+
+    private var isFabOptionsShown = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setupBottomNavigation()
+        setupFAB()
     }
 
     private fun setupBottomNavigation() {
@@ -24,6 +36,34 @@ class HomeActivity : BaseViewModelActivity<ActivityHomeBinding, HomeViewModel>()
         binding.bottomNav.setOnNavigationItemSelectedListener {
 
             return@setOnNavigationItemSelectedListener true
+        }
+    }
+
+    private fun setupFAB() {
+        binding.fabAdd.ninjaTap {
+            isFabOptionsShown = !isFabOptionsShown
+            openFabOptions(!isFabOptionsShown)
+        }
+
+    }
+
+    private fun openFabOptions(isShowFabOptions: Boolean) {
+        if (!isShowFabOptions) {
+            binding.fabIncome.visibility = View.VISIBLE
+            binding.fabExpense.visibility = View.VISIBLE
+            binding.fabOverlay.visibility = View.VISIBLE
+
+            binding.fabIncome.startAnimation(fromBottomAnim)
+            binding.fabExpense.startAnimation(fromBottomAnim)
+            binding.fabAdd.startAnimation(openAnim)
+        } else {
+            binding.fabIncome.visibility = View.GONE
+            binding.fabExpense.visibility = View.GONE
+            binding.fabOverlay.visibility = View.GONE
+
+            binding.fabIncome.startAnimation(toBottomAnim)
+            binding.fabExpense.startAnimation(toBottomAnim)
+            binding.fabAdd.startAnimation(closeAnim)
         }
     }
 
